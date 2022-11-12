e7cf41b3711e171c083f345a86cfa86256207bc1
Maksar <adminochka@gmail.com>
2022-11-12 13:18:18 +0200

    получить данные с сервера для миниатюр

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // точка входа. Модуль, который связывает другие модули
 
 import './util.js';
-import './data.js';
 import './pictures.js';
 import './photo-modal.js';
 import './form.js';
@@ -10,4 +9,8 @@ import './filter.js';
 
 import {getPictureList} from './pictures.js';
 
-getPictureList();
+fetch('https://27.javascript.pages.academy/kekstagram-simple/data')
+  .then((response) => response.json())
+  .then((pictureList) => {
+    getPictureList(pictureList);
+  });
--- js/pictures.js
+++ js/pictures.js
@@ -1,22 +1,21 @@
 // модуль, который отрисовывает миниатюры
 
-import {getPhotos} from './data.js';
-
 const pictureContainerElement = document.querySelector('.pictures');
 const pictureTemplateElement = document.querySelector('#picture').content.querySelector('.picture');
 
-const pictures = getPhotos();
+const getPictureList = (pictureList) => {
 
-const pictureContainerFragment = document.createDocumentFragment();
+  const pictureContainerFragment = document.createDocumentFragment();
 
-pictures.forEach(({url, comments, likes}) => {
-  const pictureItemElement = pictureTemplateElement.cloneNode(true);
-  pictureItemElement.querySelector('.picture__img').src = url;
-  pictureItemElement.querySelector('.picture__likes').textContent = comments;
-  pictureItemElement.querySelector('.picture__comments').textContent = likes;
-  pictureContainerFragment.append(pictureItemElement);
-});
+  pictureList.forEach(({url, comments, likes}) => {
+    const pictureItemElement = pictureTemplateElement.cloneNode(true);
+    pictureItemElement.querySelector('.picture__img').src = url;
+    pictureItemElement.querySelector('.picture__comments').textContent = comments;
+    pictureItemElement.querySelector('.picture__likes').textContent = likes;
+    pictureContainerFragment.append(pictureItemElement);
+  });
 
-const getPictureList = () => pictureContainerElement.append(pictureContainerFragment);
+  pictureContainerElement.append(pictureContainerFragment);
+};
 
 export {getPictureList};
