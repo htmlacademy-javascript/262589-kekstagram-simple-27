0487dc0262a3d40116a6290b5b7e53c71d691736
Maksar <adminochka@gmail.com>
2022-11-12 15:57:04 +0200

    Реализация сообщения, если при загрузке миниатюр с сервера произошла ошибка запроса

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
 // точка входа. Модуль, который связывает другие модули
 
-import './util.js';
+// import './util.js';
 import './pictures.js';
 import './photo-modal.js';
 import './form.js';
 import './scale.js';
 import './filter.js';
 
+import {showAlertErrorPictureList} from './util.js';
 import {closeModal} from './photo-modal.js';
 import {setUserFormSubmit} from './form.js';
 import {getPictureList} from './pictures.js';
 
 fetch('https://27.javascript.pages.academy/kekstagram-simple/data')
-  .then((response) => response.json())
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      showAlertErrorPictureList('Не удалось загрузить фотографии других пользователей.');
+    }
+  })
   .then((pictureList) => {
     getPictureList(pictureList);
+  })
+  .catch(() => {
+    showAlertErrorPictureList('Не удалось загрузить фотографии других пользователей.');
   });
 
 setUserFormSubmit(closeModal);
